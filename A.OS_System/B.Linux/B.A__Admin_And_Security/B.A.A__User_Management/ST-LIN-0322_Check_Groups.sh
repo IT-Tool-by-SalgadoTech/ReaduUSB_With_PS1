190ecf3a42eb555ccca8e58d7f0e97911be887bf
IT-Tool_By_SalgadoTech <salgadotechnologies@outlook.com>
2026-06-02 09:24:22 -0400

    hjj

---
+++ A.OS_System/B.Linux/B.A__Admin_And_Security/B.A.A__User_Management/ST-LIN-0322_Check_Groups.sh
@@ -0,0 +1,32 @@
+#!/usr/bin/env bash
+# ==============================================================
+# IT-Tool by SalgadoTech
+# Script: ST-LIN-0322_Check_Groups.sh
+# ScriptID: ST-LIN-0322
+# Version: 1.0
+# Date: 2026-06-02
+# Category: Linux > Admin And Security > User Management
+# Description: Displays all system groups, GIDs, and members.
+#              Works universally on Ubuntu, Arch, Debian, Fedora, Kali, and others.
+# (c) 2026 SalgadoTech - All Rights Reserved
+# Unauthorized distribution prohibited
+# ==============================================================
+
+GREEN='\033[0;32m'
+RED='\033[0;31m'
+YELLOW='\033[1;33m'
+NC='\033[0m'
+
+echo ""
+echo -e '\033[0;36m\033[0m'
+echo -e '\033[0;36m  _____ _____  _______ ____   ____  _\033[0m'
+echo -e '\033[0;36m |_   _|_   _||__   __/ __ \ / __ \| |\033[0m'
+echo -e '\033[0;36m   | |   | |     | | | |  | | |  | | |\033[0m'
+echo -e '\033[0;36m   | |   | |     | | | |  | | |  | | |\033[0m'
+echo -e '\033[0;36m  _| |_  | |     | | | |__| | |__| | |___\033[0m'
+echo -e '\033[0;36m |_____| |_|     |_|  \____/ \____/|_____|\033[0m'
+echo -e '\033[0;36m\033[0m'
+echo -e '\033[0;37m  ==================================================================\033[0m'
+echo -e '\033[0;36m  IT-Tool by SalgadoTech\033[0m'
+echo -e '\033[0;36m  Script: ST-LIN-0322_Check_Groups.sh\033[0m'
+echo -e '\033[0;36m  ScriptID: ST-LIN-
